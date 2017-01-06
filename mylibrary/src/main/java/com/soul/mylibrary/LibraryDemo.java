755c57679a8ba91d17b3e319a65384737ba9e439
soul <405557524@qq.com>
2017-01-06 14:30:06 +0800

    依赖library

---
+++ mylibrary/src/main/java/com/soul/mylibrary/LibraryDemo.java
@@ -0,0 +1,18 @@
+package com.soul.mylibrary;
+
+/**
+ * * @author soul
+ *
+ * @项目名:zmLibrary
+ * @包名: com.soul.mylibrary
+ * @作者：祝明
+ * @描述：TODO
+ * @创建时间：2017/1/6 14:29
+ */
+
+public class LibraryDemo {
+
+    public static  String getString(){
+        return "hello!";
+    }
+}
